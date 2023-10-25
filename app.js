69691e74b50cd11de193d143ae96e4dd40390c90
Sebastian Puerta <sepuertabe@gmail.com>
2023-10-25 15:26:19 -0500

    Included salary

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ var msg = 'Hello World';
 console.log(msg);
 console.log("My name is Sebastian Puerta");
 var age = 28;
-console.log("My age is",age);
+console.log("My age is", age);
+
+var salary = 3000;
+console.log("My salary in USD is: ", salary)
